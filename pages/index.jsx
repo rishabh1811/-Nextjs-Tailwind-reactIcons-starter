637b7b5b1b571f0a9d2bf3a2d94c2300331c9378
Rishabh Gupta <nameisrishabh@gmail.com>
2022-01-25 09:48:46 +0530

    starter

---
+++ pages/index.jsx
@@ -0,0 +1,19 @@
+
+import { FaReact, FaSass } from "react-icons/fa"
+import { SiNextdotjs, SiTailwindcss } from "react-icons/si"
+
+export default function Home() {
+  return (<>
+  <div className=" p-6 space-y-4">
+    <h1 className="text-4xl"><span className='text-sky-500 font-bold'>Tailwind</span> Starter</h1>
+    <h2 className="text-3xl underline underline-offset-4">Features</h2>
+      <ul className="text-lg">
+        <li className="flex items-center gap-x-2"><SiNextdotjs className='text-black' /> Next.js 12.0.8</li>
+        <li className="flex items-center gap-x-2"><SiTailwindcss className='text-blue-600' /> Tailwind 3.0.16</li>
+        <li className="flex items-center gap-x-2"><FaSass  className='text-pink-600' /> Sass </li>
+        <li className="flex items-center gap-x-2"><FaReact className='text-fuchsia-600' /> react-icons</li>
+      </ul>
+  </div>
+  
+  </>)
+}
